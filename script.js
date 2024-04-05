8c9fc5384ec478eb8474d6d67c90da7eb4f88676
Alex Messias <alexmessiasgt@gmail.com>
2024-04-05 09:49:40 -0300

    atualização

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ window.addEventListener('scroll', function () {
     nav.classList.remove('bg-dark', 'shadow');
   }
 });
+
+let janela = document.getElementById('janela');
+let botaoMostrar = document.getElementById('mostrar-janela');
+let botaoFechar = document.querySelector('.fechar');
+let navbar = document.querySelector ('.navbar');
+
+botaoMostrar.addEventListener('click', function() {
+  event.preventDefault();
+  document.body.classList.add('overlay-open'); // Adiciona a classe para aplicar o estilo de fundo escurecido
+  janela.style.display = 'block'; // Exibe a janela flutuante
+  navbar.style.display = 'none';
+});
+
+botaoFechar.addEventListener('click', function() {
+  document.body.classList.remove('overlay-open'); // Remove a classe para remover o estilo de fundo escurecido
+  janela.style.display = 'none'; // Oculta a janela flutuante
+  navbar.style.display = 'block';
+  console.log()
+});
