fcdb302e2489e0821900c855e4dd61e90a1540a9
Alex Messias <alexmessiasgt@gmail.com>
2024-05-29 23:27:51 -0300

    correcoes

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let nav = document.querySelector('nav');
+const togglerIcon = document.querySelector('.navbar-toggler-icon');
+const nav = document.querySelector('nav');
+let menuAberto = false; // Variável para controlar o estado do menu
+
+togglerIcon.addEventListener('click', () => {
+  menuAberto = !menuAberto; // Alterna o estado do menu
+  nav.classList.toggle('bg-dark', menuAberto);
+});
 
 window.addEventListener('scroll', function () {
-  if (window.pageYOffset > 100) {
+  if (window.pageYOffset > 100 && !menuAberto) {
+    // Verifica se o scroll está abaixo de 100 pixels e o menu não está aberto
     nav.classList.add('bg-dark', 'shadow');
   } else {
+    // Remove a classe apenas se o scroll estiver acima de 100 pixels ou o menu estiver aberto
     nav.classList.remove('bg-dark', 'shadow');
   }
 });
+
+
+
 
 let janela = document.getElementById('janela');
 let botaoMostrar = document.getElementById('mostrar-janela');
